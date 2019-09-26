8d91bf19d2d4aa68af2a4201858b35bcde145e45
siddhi vinayaka <svinayaka290489@gmail.com>
2019-09-27 00:49:23 +0530

    update files

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
+const router = express.Router();
 const PORT = process.env.PORT || 3000;
 const app = express();
-const aboutRoute = require('./controller/about-controller');
-const defaultRoute = require('./controller/default-controller');
+const aboutRoute = require('./controller/about/about-controller');
+const defaultRoute = require('./controller/default/default-controller');
 
-app.use('/', defaultRoute)
+app.use('/default', defaultRoute)
 app.use('/about', aboutRoute);
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
 
 
 app.listen(PORT, () => {
