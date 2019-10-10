2ec8a297b174e303dd2500b97fd6a5b4d04bce8c
siddhi vinayaka <svinayaka290489@gmail.com>
2019-10-10 23:54:23 +0530

    console log

--- public/BallGame/ball.js
+++ public/BallGame/ball.js
@@ -4,8 +4,6 @@ const path = require('path');
 const router = express.Router();
 
 app.set("ballGame", path.join(__dirname, "public/BallGame"));
-//app.use("ballGame", express.static(__dirname, "public/BallGame"));
-app.use(express.static(__dirname + '/public'));
 
 router.get("/", (req, res, next) => {
   
