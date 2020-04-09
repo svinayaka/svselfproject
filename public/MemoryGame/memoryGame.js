d7dd6573b11b6fd75a24ee0d886c287283b4c99f
Siddhi <svinayaka290489@gmail.com>
2020-04-09 20:10:27 +0530

    update game

--- public/MemoryGame/memoryGame.js
+++ public/MemoryGame/memoryGame.js
@@ -3,22 +3,29 @@
     function createCards() {
         var div = document.createDocumentFragment('div');
         var gameElm = document.getElementById('game');
-        var cardList = memoryList.map((eachCard) => {
-            var 
+        var cardList = memoryList.map((eachCard, id) => {
+            var card = document.createElement('div');
+            var backCard =  backCards(id+1);
+            var frontCard =  frontCards(id+1);
+            card.appendChild(backCard);
+            card.appendChild(frontCard);
+            return card;
         });
-
+        div.appendChild(cardList);
+        gameElm.appendChild(div);
     }
 
     function backCards(id) {
         var backElm = document.createElement('div');
         backElm.classList = 'back';
-        backElm.id = id;
+        backElm.id = `back${id}`
         return backElm;
     }
     function frontCards(id) {
         var frontElm = document.createElement('div');
         frontElm.classList = 'front';
-        frontElm.id = id;
+        frontElm.id = `front${id}`;
         return frontElm;
     }
+    createCards();
 })()
