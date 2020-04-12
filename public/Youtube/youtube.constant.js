eb9ee5a6176dc62bd8863f686079893d0862d6a4
Siddhi <svinayaka290489@gmail.com>
2020-04-12 21:07:50 +0530

    youtube

--- public/Youtube/youtube.constant.js
+++ public/Youtube/youtube.constant.js
@@ -1,2 +1,2 @@
-const BASEURL = 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list';
+const BASEURL = 'https://www.googleapis.com/youtube/v3/search';
 const apiKey = 'AIzaSyAIq4ZiW6OB7G6U4_hxUYAPFQNL3NVs7nU';
--- public/Youtube/youtube.js
+++ public/Youtube/youtube.js
@@ -1,12 +1,32 @@
 (function () {
     var searchUsr = {
-        part: '',
+        part: '', //snippet
         q: '',
         type: '', // channel, playlist, video
-        relevanceLanguage='',
-        videoDefinition='', // any, standard, high
+        relevanceLanguage:'',
+        videoDefinition:'', // any, standard, high
         maxResults: 5,
         order: '', //date, raing, relevance, title, videoCount, viewCount
     }
+    var userInput = document.getElementById('youtube-searchInput');
+    var userSearch = document.getElementById('youtube-searchBtn');
+
+    userSearch.addEventListener('click', (evet) => {
+        youtubeSearch();
+    });
+
+    function youtubeSearch() {
+        fetch(BASEURL + '?' + 'part=snippet&q=test&key=AIzaSyAIq4ZiW6OB7G6U4_hxUYAPFQNL3NVs7nU&maxResults=20',{
+            mode: "no-cors",
+            method: 'GET',
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials" : true 
+        })
+        .then((resp) => resp.json())
+        .then(resp => {
+            debugger;
+        })
+    }
+
 }())
 
