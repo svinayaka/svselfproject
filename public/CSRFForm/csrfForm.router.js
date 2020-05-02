90717aa7273b820d5e707d888bb4f83796790f47
Siddhi Vinayaka <siddhi.vinayaka@bhge.com>
2020-05-02 13:18:53 +0530

    update csrf project

--- public/CSRFForm/csrfForm.router.js
+++ public/CSRFForm/csrfForm.router.js
@@ -8,8 +8,8 @@ app.use('CSRFForm', express.static('/public/CSRFForm'));
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '/csrfForm.html'));
 });
-router.get('/csrfLogIn', (req, res, next) => {
-    res.sendFile(path.join(__direname, '/csrfFormLoggedIn.html'));
+router.post('/csrfLogIn', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/csrfFormLoggedIn.html'));
 })
 
 module.exports = router;
