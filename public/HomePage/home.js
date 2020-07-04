e92aab5efaccd3f4cb335831a216ca930845e3a3
siddhi vinayaka <svinayaka290489@gmail.com>
2020-07-05 04:24:05 +0530

    redirecting home page to dashboard

--- public/HomePage/home.js
+++ public/HomePage/home.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path")
+const path = require("path");
+var compression = require('compression');
 const app = express();
 
-app.use("Home", express.static("/public/HomePage"));
+app.use(compression());
+app.use('Dashboard', express.static('/public/DashBoard'));
+// app.use("Home", express.static("/public/HomePage"));
 
 router.get("/", (req, res, next) => {
-    res.render(path.join(__dirname, "/home"));
-    //res.sendFile(path.join(__dirname, "/home.html"));
+    // res.render(path.join(__dirname, "/home"));
+    // using alternate
+    const newPath = __dirname.split('public')[0] ;
+    res.sendFile(path.join(newPath, "/public/DashBoard/dashboard.html"));
 })
 
 module.exports = router;
