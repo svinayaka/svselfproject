49b6243b8c6bcf5b203cd7e0ea44415462cedb67
siddhi vinayaka <svinayaka290489@gmail.com>
2020-07-16 10:33:45 +0530

    update file

--- public/SpeedType/speedtype.js
+++ public/SpeedType/speedtype.js
@@ -1,7 +1,6 @@
 var speedType = (function() {
     function SpeedType() {
         SpeedType.prototype.renderDOMSelectComponent = function(id, options, labelId) {
-            // settings-select
             var documentFrag = document.createDocumentFragment();
             var selectElm = document.createElement('select');
             var selectLabelElm = document.createElement('label');
@@ -19,6 +18,36 @@ var speedType = (function() {
             documentFrag.appendChild(selectElm);
             return documentFrag;
         }
+        SpeedType.prototype.renderDOMStartType = function(idName) {
+            var list = [{ id:'settings-timer', txt:'Time Left'}, { id:'settings-score', txt:'Score'}];
+            var documentFrag = document.createDocumentFragment();
+            var timeStartElm = document.createElement('section');
+            timeStartElm.id = idName;
+            timeStartElm.classList.add(idName);
+            var headerElm = document.createElement('h2');
+            headerElm.innerHTML = 'Speed Typer';
+            timeStartElm.appendChild(headerElm);
+            list.forEach((eachList) => {
+                timeStartElm.appendChild(generateStartGameInfo(eachList));
+            });
+            var paraElm = document.createElement('p');
+            paraElm.id = 'settings-following';
+            paraElm.classList.add('settings-following');
+            paraElm.innerHTML = 'Type the following:';
+            timeStartElm.appendChild(paraElm);
+            var inputElm = document.createElement('input');
+            inputElm.id = 'settings-typeArea';
+            inputElm.classList.add('settings-typeArea');
+            inputElm.placeholder = 'Enter the word';
+            timeStartElm.appendChild(inputElm);
+            // finally append...
+            documentFrag.appendChild(timeStartElm);
+            return documentFrag;
+        }
+        SpeedType.prototype.renderDOMEndType = function(id) {
+            
+            
+        }
     }
     return new SpeedType();
 })() || null;
@@ -27,6 +56,7 @@ if (speedType) {
     (function() {
         var selectFormContainer = document.getElementById('settings-form-container');
         var selectContainer = document.getElementById('settings-select-container');
+        var settingsContainer = document.getElementById('settings-timer-container');
         var settingsbtn = document.getElementById('settings-btn');
         settingsbtn.addEventListener('click', onSettingClicked.bind(this, selectFormContainer))
         var selectOptions = [
@@ -36,14 +66,15 @@ if (speedType) {
         ];
         clearDOM(selectContainer);
         selectContainer.appendChild(speedType.renderDOMSelectComponent('settings-select', selectOptions, 'settings-Label'));
+        settingsContainer.appendChild(speedType.renderDOMStartType('settings-timestart'));
+        // settingsContainer.appendChild(speedType.renderDOMStartType('settings-timeranout'));
     })();
 }
 //---------default binding-------------//
 function onSelectionChanged(event) {
-
+    
 }
 function onSettingClicked(elm) {
-    debugger;
     if (elm.classList.contains('settings-form-container-hide')) {
         elm.classList.remove('settings-form-container-hide');
         elm.classList.add('settings-form-container-show');
@@ -53,6 +84,18 @@ function onSettingClicked(elm) {
     }
 }
 //-------------------------------------//
+function generateStartGameInfo(list) {
+    var textElm = document.createTextNode(list.txt);
+    var paraElm = document.createElement('p');
+    var spanElm = document.createElement('span');
+    paraElm.id = list.id;
+    paraElm.classList.add(list.id);
+    spanElm.id = list.id+'info';
+    spanElm.classList.add(list.id+'info');
+    paraElm.appendChild(textElm);
+    paraElm.appendChild(spanElm);
+    return paraElm;
+}
 //---------------------//
 function generateOptions(option, index) {
     var optElm = document.createElement('OPTION');
