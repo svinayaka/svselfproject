b8da6df0aa0d16596e5d85afcecaf102c801d5e6
siddhi vinayaka <svinayaka290489@gmail.com>
2020-10-04 01:21:15 +0530

    update file

--- public/WildShootOut/wildshootout.js
+++ public/WildShootOut/wildshootout.js
@@ -1,18 +1,34 @@
 // https://subscription.packtpub.com/video/web_development/9781789801385/79426/79432/game-prep-style-your-game
-var lastHideOut = null;
+var lastHideOut = [];
 var gameOver = false;
+var clearStartedGame = null;
 var score = 0;
 var gameBoardCards = 8;
+var tries = 3;
+var popNumber = 2; 
 
 var startBtn = document.getElementById('wildShootOut__gameStart');
 var endBtn = document.getElementById('wildShootOut__gameEnd');
+var audioElm = document.getElementById('playAudio');
 
 var gameBoard = document.getElementById('wildShootOut__board');
 
 startBtn.addEventListener('click', startGame);
+endBtn.addEventListener('click', endGame)
 
 function startGame() {
-    createGameBoard()
+    audioElm.play();
+    createGameBoard();
+    startGameBoard();
+    startBtn.disabled = true;
+    endBtn.disabled = false;
+    gameOver = false;
+}
+
+function endGame() {
+    startBtn.disabled = false;
+    endBtn.disabled = true;
+    gameOver = true;
 }
 
 function createGameBoard() {
@@ -23,8 +39,12 @@ function createGameBoard() {
         divElm.className = "wildShootOut__boardCards";
         var divFriend = document.createElement('div');
         divFriend.setAttribute('class', 'wildShootOut__boardCards__friend');
+        divFriend.dataset.target = 'good';
+        divFriend.addEventListener('click', shootout);
         var divEnemy = document.createElement('div');
         divEnemy.setAttribute('class', 'wildShootOut__boardCards__bad');
+        divEnemy.dataset.target = 'bad';
+        divEnemy.addEventListener('click', shootout);
         var divBrick = document.createElement('div');
         divBrick.setAttribute('class', 'wildShootOut__boardCards__brick');
         divElm.appendChild(divFriend);
@@ -33,4 +53,57 @@ function createGameBoard() {
         frag.appendChild(divElm);
     }
     gameBoard.appendChild(frag);
+}
+
+function shootout(event) {
+    var shootType = event.target.dataset.target;
+    (shootType == 'good') ? ++score : --tries; 
+    if (tries <= 0) {
+        endGame();
+    } 
+    updateScoreBoard();
+}
+
+function updateScoreBoard() {
+    
+}
+
+function startGameBoard() {
+    randomCardPicker();
+}
+function intervalBasedCard(cardId, cardElm) {
+    if (cardId >= 0 && cardElm) {
+        var cardToShow = Math.floor(Math.random() * Math.floor(cardElm.children.length - 1));
+        var cardDisplayed = cardElm.children[cardToShow];
+        var className = (!cardDisplayed.className.includes('--show')) ?cardDisplayed.className + '--show' : cardDisplayed.classList[cardDisplayed.classList.length-1];
+        cardDisplayed.classList.add(className);
+        (function(clsName, elmId, elm) {
+            clearStartedGame = setTimeout(function() {
+                elm.classList.remove(clsName);
+                //var index = lastHideOut.indexOf(elmId);
+                //lastHideOut.splice(index, 1);
+                var maxExecute = (lastHideOut.length > 1) ? 1 : Math.floor(Math.random() * (3 - 1) + 1);
+                if (!gameOver) {
+                    randomCardPicker();
+                } else {
+                    clearTimeout(clearStartedGame);
+                }
+            }, 700);
+        })(className, cardId, cardDisplayed);
+    }
+}
+
+function randomCardPicker() {
+    var gameBoard = document.querySelectorAll('.wildShootOut__boardCards');
+    var currentCard = Math.floor(Math.random() * Math.floor(gameBoard.length));
+    var firstId;
+    var cardElm;
+    if (lastHideOut.includes(currentCard)) {
+        randomCardPicker();
+    } else {
+        lastHideOut.push(currentCard);
+    }
+    firstId = lastHideOut.shift();
+    cardElm = gameBoard[firstId];
+    intervalBasedCard(firstId, cardElm);
 }
--- public/WildShootOut/wildshootout.router.js
+++ public/WildShootOut/wildshootout.router.js
@@ -3,9 +3,22 @@ const app = express();
 const router = express.Router();
 const path = require("path");
 const compression = require('compression');
+const fs = require('fs');
 
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '/wildshootout.html'));
+});
+router.get('/friendImg', (req, res, next) => {
+    res.sendFile(path.join(__dirname,'/images/citizen.png'));
+});
+router.get('/enemyImg', (req, res, next) => {
+    res.sendFile(path.join(__dirname,'/images/terrorist2.png'));
+});
+router.get('/brickImg', (req, res, next) => {
+    res.sendFile(path.join(__dirname,'/images/bricks.jpg'));
+});
+router.get('/music', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/music/Illusionary_Daytime.mp3'));
 });
 
 module.exports = router;
